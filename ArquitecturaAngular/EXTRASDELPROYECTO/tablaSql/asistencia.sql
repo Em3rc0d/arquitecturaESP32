78d803bf08e7a5b123787709a8f8403ceb593849
Eduardo <farid.merino1673@gmail.com>
2024-06-29 23:28:42 -0500

    functional

--- ArquitecturaAngular/EXTRASDELPROYECTO/tablaSql/asistencia.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE asistencia (
-    id SERIAL PRIMARY KEY,
-    codigo_alumno VARCHAR(50) NOT NULL,
-    estado VARCHAR(20) NOT NULL DEFAULT 'no asistió',
-    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
